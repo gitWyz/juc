4738437ef26bca3cb22b5e5654226190ae32d0ed
wangyizhong <wangyizhong@wusongtech.com>
2021-03-05 15:39:14 +0800

    init

---
+++ src/main/java/com/wyz/Test.java
@@ -0,0 +1,13 @@
+package com.wyz;
+
+/**
+ * Test
+ *
+ * @author wangyz
+ * @date 2021/3/5 15:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
